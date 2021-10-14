bdac8b9a6478d4671010cc9a406ea16681a830f4
TEST <example@mail.ru>
2021-10-14 11:19:43 +0200

    Task 6.1

---
+++ Task 6.1.java
@@ -0,0 +1,10 @@
+package com.company;
+
+    public class Main {
+
+        public static void main(String[] args) {
+            int a = 3;
+            int b = 2;
+            System.out.println("sumOfTwoNumbers" + "=" + (a+b));
+        }
+    }
